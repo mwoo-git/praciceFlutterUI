498cbed6c9cd833b72741b33377d132d5afbc486
mwoo-git <hanulbom@gmail.com>
2023-07-31 02:13:10 +0900

    탑 헤더 생성

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[800],
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              TopHeader(),
+
+              // search bar
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TopHeader extends StatelessWidget {
+  const TopHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        // 안녕 민우
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('안녕 민우!',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold)),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              '2023년 7월 31일',
+              style: TextStyle(color: Colors.blue[200]),
+            )
+          ],
+        ),
+
+        // Notification
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.blue[600],
+              borderRadius: BorderRadius.circular(12)),
+          padding: const EdgeInsets.all(12),
+          child: const Icon(
+            Icons.notifications,
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
+  }
+}
