45ac8928898d1fcb31c885d1f12f35afcf264e9d
mwoo-git <hanulbom@gmail.com>
2023-08-04 21:59:42 +0900

    add: 엑서사이즈 타이틀 분리 직전까지 완료

--- lib/pages/home/view/home_page.dart
+++ lib/pages/home/view/home_page.dart
@@ -51,7 +51,80 @@ class _HomePageState extends State<HomePage> {
             ),
             Expanded(
               child: Container(
-                color: Colors.grey[100],
+                padding: EdgeInsets.all(25),
+                color: Colors.grey[200],
+                child: Center(
+                  child: Column(
+                    children: [
+                      // exercise heading
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Excercises',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Icon(Icons.more_horiz)
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      // list
+                      Container(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(16)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Container(
+                                        padding: EdgeInsets.all(16),
+                                        color: Colors.orange,
+                                        child: Icon(Icons.favorite,
+                                        color: Colors.white,)),
+                                  ),
+                                  SizedBox(
+                                    width: 12,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '말하기 스킬들',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                      SizedBox(width: 5),
+                                      Text(
+                                        '16 Exercises',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey,
+                                          fontSize: 14,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Icon(Icons.more_horiz)
+                            ],
+                          ))
+                    ],
+                  ),
+                ),
               ),
             )
           ],
