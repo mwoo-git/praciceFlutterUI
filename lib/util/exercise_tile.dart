4afa1465ead1ba098bc177adc4f4afa8226b805e
mwoo-git <hanulbom@gmail.com>
2023-08-05 21:31:32 +0900

    add:  불필요한 코드 삭제

--- lib/util/exercise_tile.dart
+++ lib/util/exercise_tile.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ExerciseTile extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
   final icon;
   final String exerciseName;
   final int numberOfExercises;
+  // ignore: prefer_typing_uninitialized_variables
   final color;
 
   const ExerciseTile(
